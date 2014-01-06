729d94b8470378ebbb801d72533f60711e039e93
Roman <felixcon@gmail.com>
2014-01-07 00:16:37 +0400

    initial commit

---
+++ src/rblog/routes/home.clj
@@ -0,0 +1,15 @@
+(ns rblog.routes.home
+  (:use compojure.core)
+  (:require [rblog.views.layout :as layout]
+            [rblog.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
